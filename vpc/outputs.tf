50688a6d0d46bf75b7c9784a8dedc78e09f32da9
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-02 23:34:35 -0500

    added output

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,14 +1,34 @@
 output "vpc_id" {
-  description = "ID of the VPC"
+  description = "The ID of the VPC"
   value       = aws_vpc.terraform-vpc.id
 }
 
-output "vpc_cidr_block" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.terraform-vpc.cidr_block
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
-output "vpc_arn" {
-  description = "ARN of the VPC"
-  value       = aws_vpc.terraform-vpc.arn
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = aws_nat_gateway.nat.id
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "The ID of the private route table"
+  value       = aws_route_table.private.id
 }
