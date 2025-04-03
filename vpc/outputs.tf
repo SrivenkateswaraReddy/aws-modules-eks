52adbecfb3336673c2f1aa6332079886327d0a1e
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-03 14:13:00 -0500

    added outputs

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,34 +1,44 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "ID of the created VPC"
   value       = aws_vpc.terraform-vpc.id
 }
 
-output "public_subnets" {
-  description = "List of public subnet IDs"
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
   value       = aws_subnet.public[*].id
 }
 
-output "private_subnets" {
-  description = "List of private subnet IDs"
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
   value       = aws_subnet.private[*].id
 }
 
 output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
+  description = "ID of the Internet Gateway"
   value       = aws_internet_gateway.igw.id
 }
 
 output "nat_gateway_id" {
-  description = "The ID of the NAT Gateway"
+  description = "ID of the NAT Gateway"
   value       = aws_nat_gateway.nat.id
 }
 
 output "public_route_table_id" {
-  description = "The ID of the public route table"
+  description = "ID of the public route table"
   value       = aws_route_table.public.id
 }
 
 output "private_route_table_id" {
-  description = "The ID of the private route table"
+  description = "ID of the private route table"
   value       = aws_route_table.private.id
+}
+
+output "public_nacl_id" {
+  description = "ID of the public Network ACL"
+  value       = aws_network_acl.public_nacl.id
+}
+
+output "private_nacl_id" {
+  description = "ID of the private Network ACL"
+  value       = aws_network_acl.private_nacl.id
 }
