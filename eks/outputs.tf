b961b988fd86c2dd5c20ae81e69b2f0fd58b319b
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-05 18:54:26 -0500

    updated outputs

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,19 +1,29 @@
-output "eks_cluster_id" {
-  value       = aws_eks_cluster.this.id
-  description = "EKS Cluster ID"
+output "eks_cluster_name" {
+  value       = aws_eks_cluster.eks_cluster.name
+  description = "The name of the EKS cluster"
+}
+
+output "eks_cluster_arn" {
+  value       = aws_eks_cluster.eks_cluster.arn
+  description = "The ARN of the EKS cluster"
 }
 
 output "eks_cluster_endpoint" {
-  value       = aws_eks_cluster.this.endpoint
-  description = "EKS API Server endpoint"
+  value       = aws_eks_cluster.eks_cluster.endpoint
+  description = "The endpoint URL of the EKS cluster"
 }
 
-output "eks_cluster_ca_data" {
-  value       = aws_eks_cluster.this.certificate_authority[0].data
-  description = "Certificate authority data for the cluster"
+output "eks_node_group_name" {
+  value       = aws_eks_node_group.eks_nodes.node_group_name
+  description = "The name of the EKS node group"
 }
 
-output "eks_node_group_id" {
-  value       = aws_eks_node_group.this.id
-  description = "EKS Node Group ID"
+output "eks_node_group_instance_types" {
+  value       = aws_eks_node_group.eks_nodes.instance_types
+  description = "The EC2 instance types used in the EKS node group"
+}
+
+output "referenced_eks_cluster_name" {
+  value       = data.terraform_remote_state.eks.outputs.eks_cluster_name
+  description = "The name of the referenced EKS cluster"
 }
