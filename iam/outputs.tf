4e7b23571b20474a7cf83d6c0fb932b87714639c
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-05 23:29:13 -0500

    updated-eks

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -7,3 +7,8 @@ output "eks_node_role_arn" {
   description = "IAM role ARN for EKS nodes"
   value       = aws_iam_role.eks_node_role.arn
 }
+
+
+output "eks_node_role_arn" {
+  value = aws_iam_role.eks_node_role.arn
+}
