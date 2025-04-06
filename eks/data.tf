f259b525265866859a05aedadcf223cd8a92f3ce
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-05 23:22:42 -0500

    updated eks

--- eks/data.tf
+++ eks/data.tf
@@ -15,3 +15,24 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.dev-eks-cluster.name
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = <<YAML
+    - rolearn: ${aws_iam_role.eks_nodes.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    YAML
+  }
+}
