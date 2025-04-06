a3ed7cd52a8cab9e350f4d21755f416900f8ef5b
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-05 19:14:52 -0500

    updated data block

--- eks/data.tf
+++ eks/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "iam" {
   backend = "s3"
   config = {
-    bucket = var.s3_bucket_details
+    bucket = "my-aws-terraform-s3-backend-vicky"
     key    = "modules/iam/terraform.tfstate"
     region = "us-east-1"
   }
@@ -10,7 +10,7 @@ data "terraform_remote_state" "iam" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = var.s3_bucket_details
+    bucket = "my-aws-terraform-s3-backend-vicky"
     key    = "modules/vpc/terraform.tfstate"
     region = "us-east-1"
   }
