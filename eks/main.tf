c24b737812a8f347d2376c94bb2c6c4b22126cd2
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-11 19:42:41 -0500

    updated-eks

--- eks/main.tf
+++ eks/main.tf
@@ -5,23 +5,23 @@ module "eks" {
   cluster_version = var.cluster_version
 
   vpc_id                          = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids                      = data.terraform_remote_state.vpc.outputs.public_subnet_ids
-  control_plane_subnet_ids        = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+  subnet_ids                      = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  control_plane_subnet_ids        = data.terraform_remote_state.vpc.outputs.private_subnet_ids
   cluster_endpoint_public_access  = var.cluster_endpoint_public_access
   cluster_endpoint_private_access = var.cluster_endpoint_private_access
   enable_irsa                     = true
   create_iam_role                 = false
-  # iam_role_name                         = var.iam_role_name
+  iam_role_name                         = var.iam_role_name
   cluster_ip_family                     = var.cluster_ip_family
   iam_role_arn                          = local.eks_cluster_role_arn
   cluster_security_group_id             = aws_security_group.eks_cluster_sg.id
   cluster_additional_security_group_ids = [aws_security_group.eks_node_sg.id]
 
 
-  # cluster_compute_config = {
-  #   enabled    = var.cluster_compute_config_enabled
-  #   node_pools = var.cluster_node_pools
-  # }
+  cluster_compute_config = {
+    enabled    = var.cluster_compute_config_enabled
+    node_pools = var.cluster_node_pools
+  }
 
   eks_managed_node_groups = {
     default = {
@@ -32,15 +32,15 @@ module "eks" {
       instance_types = var.node_group_instance_types
       capacity_type  = var.node_group_capacity_type
 
-      # ami_type  = var.node_group_ami_type
-      # disk_size = var.node_group_disk_size
+      ami_type  = var.node_group_ami_type
+      disk_size = var.node_group_disk_size
 
-      # labels = var.node_group_labels
+      labels = var.node_group_labels
 
-      # taints = var.node_group_taints
+      taints = var.node_group_taints
 
-      #   create_iam_role = false
-      #   iam_role_arn    = local.eks_node_role_arn
+        create_iam_role = false
+        iam_role_arn    = local.eks_node_role_arn
       tags = merge(var.tags,
         {
           Name = "tfe_vpc"
