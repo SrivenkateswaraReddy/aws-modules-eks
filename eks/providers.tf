ac3f5ca5879ff7181b178b6b82a3dbc83019ef0a
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-20 17:19:51 -0500

    updated-eks-provider

--- eks/providers.tf
+++ eks/providers.tf
@@ -21,12 +21,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.dev-eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.dev-eks-cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.dev-eks-cluster.name]
+ provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+ provider "kubectl" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
 }
