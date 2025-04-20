01f3e4b37b07bb4d38975e0648f33c0539551e31
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-20 17:02:20 -0500

    updated-providers

--- eks/providers.tf
+++ eks/providers.tf
@@ -21,12 +21,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# provider "kubernetes" {
-#   host                   = aws_eks_cluster.dev-eks-cluster.id                          # Or data source/variable
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data) # Or data source/variable
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id] # Or data source/variable
-#   }
-# }
+provider "kubernetes" {
+  host                   = aws_eks_cluster.dev-eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.dev-eks-cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.dev-eks-cluster.name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.dev-eks-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.dev-eks-cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.dev-eks-cluster.name]
+    }
+  }
+}
+
