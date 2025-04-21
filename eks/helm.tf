ef9fd7fb5b8593054fa9d918380422565e6c6b7b
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-21 16:28:25 -0500

    updated-helm-tf

--- eks/helm.tf
+++ eks/helm.tf
@@ -1,32 +1,23 @@
-resource "kubernetes_namespace" "grafana" {
-  metadata {
-    name = "grafana"
-  }
-}
+
 
 resource "helm_release" "grafana" {
-  name       = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "grafana"
-  namespace  = kubernetes_namespace.grafana.metadata[0].name
-  version    = "8.12.1" # Replace with the desired chart version
-
-  values = [file("${path.module}/variables/grafana-values.yaml")]
+  name             = "grafana"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana"
+  namespace        = "grafana"
+  version          = "8.12.1" # Replace with the desired chart version
+  create_namespace = true
+  values           = [file("${path.module}/variables/grafana-values.yaml")]
 }
 
-resource "kubernetes_namespace" "prometheus" {
-  metadata {
-    name = "prometheus"
-  }
-}
 
 resource "helm_release" "prometheus" {
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "prometheus"
-  namespace        = kubernetes_namespace.prometheus.metadata[0].name
+  namespace        = "prometheus"
   version          = "27.11.0" # Replace with the desired chart version
-  create_namespace = false
+  create_namespace = true
 
   values = [file("${path.module}/variables/prometheus-values.yaml")]
 }
