233711d51a10d9ae245c8337e4bebda1f005a3d0
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-21 15:42:00 -0500

    removed unwanted

--- eks/providers.tf
+++ eks/providers.tf
@@ -27,9 +27,3 @@ provider "helm" {
     }
   }
 }
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.dev-eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.dev-eks-cluster.certificate_authority[0].data)
-  token                  = aws_eks_cluster_auth.dev-eks-cluster.name
-}
