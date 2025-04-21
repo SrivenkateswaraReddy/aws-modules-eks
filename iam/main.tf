644606a538a789e0cac15ac7fd5cda7094a42290
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-21 16:49:14 -0500

    updated-eks role policy

--- iam/main.tf
+++ iam/main.tf
@@ -77,3 +77,8 @@ resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
