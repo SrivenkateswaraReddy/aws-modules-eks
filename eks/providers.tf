6bd993fa76d4a51c298216847acd1fef94ee4d9a
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-21 15:35:16 -0500

    updated-provider

--- eks/providers.tf
+++ eks/providers.tf
@@ -27,3 +27,9 @@ provider "helm" {
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.dev-eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.dev-eks-cluster.certificate_authority[0].data)
+  token                  = aws_eks_cluster_auth.dev-eks-cluster.token
+}
