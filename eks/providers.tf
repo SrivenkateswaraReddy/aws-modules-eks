baf1943b0a4b39454233befd34a7200670386bea
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-21 11:14:27 -0500

    eks-reverted

--- eks/providers.tf
+++ eks/providers.tf
@@ -4,31 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.94.1"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.36.0"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.0.0-pre2"
-    }
   }
   backend "s3" {}
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
- provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
- provider "kubectl" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
 }
