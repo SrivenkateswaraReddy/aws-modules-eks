a87441c756619befbdfa666db29ba0b8ee2c31ea
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-24 00:56:07 -0500

    removed-helm

--- eks/helm.tf
+++ eks/helm.tf
@@ -32,23 +32,23 @@
 # }
 
 
-resource "helm_release" "grafana" {
-  name             = "grafana"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "grafana"
-  namespace        = "grafana"
-  version          = "8.12.1" # Replace with the desired chart version
-  create_namespace = true
-  values           = [file("${path.module}/variables/grafana-values.yaml")]
-}
+# resource "helm_release" "grafana" {
+#   name             = "grafana"
+#   repository       = "https://grafana.github.io/helm-charts"
+#   chart            = "grafana"
+#   namespace        = "grafana"
+#   version          = "8.12.1" # Replace with the desired chart version
+#   create_namespace = true
+#   values           = [file("${path.module}/variables/grafana-values.yaml")]
+# }
 
-resource "helm_release" "prometheus" {
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
-  namespace        = "prometheus"
-  version          = "27.11.0" # Replace with the desired chart version
-  create_namespace = true
+# resource "helm_release" "prometheus" {
+#   name             = "prometheus"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   chart            = "prometheus"
+#   namespace        = "prometheus"
+#   version          = "27.11.0" # Replace with the desired chart version
+#   create_namespace = true
 
-  values = [file("${path.module}/variables/prometheus-values.yaml")]
-}
+#   values = [file("${path.module}/variables/prometheus-values.yaml")]
+# }
