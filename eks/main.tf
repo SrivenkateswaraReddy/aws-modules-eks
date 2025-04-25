730aa10c0573d1eeab9ad765334d83b599fcda41
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-04-24 23:30:06 -0500

    updated-eks

--- eks/main.tf
+++ eks/main.tf
@@ -44,38 +44,38 @@ resource "aws_eks_cluster" "dev-eks-cluster" {
 }
 
 
-# resource "aws_eks_node_group" "system" {
-#   cluster_name    = aws_eks_cluster.dev-eks-cluster.name
-#   node_group_name = "graviton"
-#   node_role_arn   = data.terraform_remote_state.iam.outputs.eks_node_role_arn
+resource "aws_eks_node_group" "system" {
+  cluster_name    = aws_eks_cluster.dev-eks-cluster.name
+  node_group_name = "graviton"
+  node_role_arn   = data.terraform_remote_state.iam.outputs.eks_node_role_arn
 
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
 
-#   scaling_config {
-#     desired_size = 0
-#     max_size     = 2
-#     min_size     = 0
-#   }
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 0
+  }
 
-#   update_config {
-#     max_unavailable = 1
-#   }
+  update_config {
+    max_unavailable = 1
+  }
 
-#   #   instance_types = ["t3.medium"]
-#   #   ami_type       = "AL2_x86_64"
-#   #   disk_size      = 20
+  #   instance_types = ["t3.medium"]
+  #   ami_type       = "AL2_x86_64"
+  #   disk_size      = 20
 
-#   #   instance_types = ["t3.small"]
-#   #   ami_type       = "AL2_x86_64"
-#   #   disk_size      = 20
+  #   instance_types = ["t3.small"]
+  #   ami_type       = "AL2_x86_64"
+  #   disk_size      = 20
 
 
-#   instance_types = [var.graviton_instance_type]
-#   ami_type       = var.ami_type_graviton
-#   disk_size      = var.node_disk_size
+  instance_types = [var.graviton_instance_type]
+  ami_type       = var.ami_type_graviton
+  disk_size      = var.node_disk_size
 
-#   tags = var.tags_all
-# }
+  tags = var.tags_all
+}
 
 resource "aws_eks_node_group" "general" {
   cluster_name    = aws_eks_cluster.dev-eks-cluster.name
@@ -85,7 +85,7 @@ resource "aws_eks_node_group" "general" {
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
 
   scaling_config {
-    desired_size = 0
+    desired_size = 1
     max_size     = 2
     min_size     = 0
   }
