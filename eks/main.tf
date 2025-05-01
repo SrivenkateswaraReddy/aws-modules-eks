e118649d66f63936f08923fc7bacc3f06757e764
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-05-01 15:36:29 -0400

    max pods in clusters

--- eks/main.tf
+++ eks/main.tf
@@ -161,3 +161,22 @@ resource "aws_eks_addon" "addons" {
   addon_name    = each.value.name
   addon_version = each.value.version
 }
+
+
+# Max pod settings
+resource "aws_launch_template" "t3_medium_custom" {
+  name_prefix   = "eks-custom-t3medium-"
+  instance_type = "t3.medium"
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    /etc/eks/bootstrap.sh ${aws_eks_cluster.dev-eks-cluster.name} \
+      --kubelet-extra-args '--max-pods=110'
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = var.tags_all
+  }
+}
