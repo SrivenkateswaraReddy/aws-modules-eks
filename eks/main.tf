24f7585b35759cd6664613a8e105ec44edb7bfac
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-05-09 01:08:55 -0400

    reverted-changes

--- eks/main.tf
+++ eks/main.tf
@@ -94,25 +94,19 @@ resource "aws_eks_node_group" "general" {
     max_unavailable = 1
   }
 
-  # instance_types = ["t3.medium"]
-  ami_type = "AL2_x86_64"
-  # ami_type = "AL2023_x86_64"
-
-  # disk_size = 20
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+  disk_size      = 20
 
   capacity_type = "SPOT"
   #   instance_types = ["t3.small"]
   #   ami_type       = "AL2_x86_64"
   #   disk_size      = 20
 
+
   # instance_types = [var.graviton_instance_type]
   # ami_type       = var.ami_type_graviton
   # disk_size      = var.node_disk_size
-
-  launch_template {
-    id      = aws_launch_template.al2023_custom.id
-    version = "$Latest"
-  }
 
   tags = var.tags_all
 }
@@ -168,28 +162,21 @@ resource "aws_eks_addon" "addons" {
   addon_version = each.value.version
 }
 
-resource "aws_launch_template" "al2023_custom" {
-  name_prefix   = "eks-custom-al2023-"
-  instance_type = "t3.medium"
 
-  block_device_mappings {
-    device_name = "/dev/xvda"
+# # Max pod settings
+# resource "aws_launch_template" "t3_medium_custom" {
+#   name_prefix   = "eks-custom-t3medium-"
+#   instance_type = "t3.medium"
 
-    ebs {
-      volume_size = 20
-      volume_type = "gp3"
-    }
-  }
+#   user_data = base64encode(<<-EOF
+#     #!/bin/bash
+#     /etc/eks/bootstrap.sh ${aws_eks_cluster.dev-eks-cluster.name} \
+#       --kubelet-extra-args '--max-pods=110'
+#   EOF
+#   )
 
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    /etc/eks/bootstrap.sh ${aws_eks_cluster.dev-eks-cluster.name} \
-      --set-kubelet-configuration='{"maxPods": 110}'
-  EOF
-  )
-
-  tag_specifications {
-    resource_type = "instance"
-    tags          = var.tags_all
-  }
-}
+#   tag_specifications {
+#     resource_type = "instance"
+#     tags          = var.tags_all
+#   }
+# }
