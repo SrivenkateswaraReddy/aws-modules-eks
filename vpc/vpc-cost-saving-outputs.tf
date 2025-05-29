2086e14217843ed62f91b4f418a2356a935b39c9
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-05-29 19:17:19 -0400

    updated-vpc

---
+++ vpc/vpc-cost-saving-outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "nat_instance_id" {
+  description = "ID of the NAT instance"
+  value       = aws_instance.nat.id
+}
+
+output "nat_instance_eni" {
+  description = "Primary network interface ID of the NAT instance"
+  value       = aws_instance.nat.primary_network_interface_id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table"
+  value       = aws_route_table.private.id
+}
