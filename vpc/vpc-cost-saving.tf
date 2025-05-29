062804cc9f06985f8298a0b75fee56fe347fa3a9
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-05-29 19:14:01 -0400

    updated-vpc

--- vpc/vpc-cost-saving.tf
+++ vpc/vpc-cost-saving.tf
@@ -1,54 +1,51 @@
-resource "aws_vpc" "terraform-vpc" {
+resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = merge(var.tags,
-    {
-      Name = "tfe_vpc"
-    }
-  )
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "eks-cost-optimized-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.terraform-vpc.id
-  tags = merge(var.tags,
-    {
-      Name = "main-igw"
-    }
-  )
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "eks-igw"
+  }
 }
 
-# Two public subnets in different AZs
 resource "aws_subnet" "public" {
   count                   = 2
-  vpc_id                  = aws_vpc.terraform-vpc.id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.${count.index + 1}.0/24"
   availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
-  tags = merge(var.tags,
-    {
-      Name = "public-subnet-${count.index + 1}"
-    }
-  )
+
+  tags = {
+    Name                     = "eks-public-subnet-${count.index + 1}"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
-# Two private subnets in different AZs
 resource "aws_subnet" "private" {
   count             = 2
-  vpc_id            = aws_vpc.terraform-vpc.id
-  cidr_block        = "10.0.${count.index + 3}.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 11}.0/24"
   availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
-  tags = merge(var.tags,
-    {
-      Name = "private-subnet-${count.index + 1}"
-    }
-  )
+
+  tags = {
+    Name                              = "eks-private-subnet-${count.index + 1}"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 }
 
+# NAT instance to replace expensive NAT Gateway
 resource "aws_security_group" "nat_sg" {
-  name        = "nat-instance-sg"
-  description = "Allow NAT instance traffic"
-  vpc_id      = aws_vpc.terraform-vpc.id
+  name        = "nat-sg"
+  description = "Allow NAT traffic"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 0
@@ -63,69 +60,49 @@ resource "aws_security_group" "nat_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = merge(var.tags,
-    {
-      Name = "nat-instance-sg"
-    }
-  )
 }
 
-resource "aws_eip" "nat" {
-  domain     = "vpc"
-  depends_on = [aws_internet_gateway.igw]
-  tags = merge(var.tags,
-    {
-      Name = "tfe-epi-nat"
-    }
-  )
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 }
 
-resource "aws_instance" "nat_instance" {
-  ami                    = "ami-00a9d4a05375b2763"
-  instance_type          = "t3.nano"
-  subnet_id              = aws_subnet.public[0].id
-  source_dest_check      = false
-  vpc_security_group_ids = [aws_security_group.nat_sg.id]
-  tags = merge(var.tags,
-    {
-      Name = "nat-instance"
-    }
-  )
-}
+resource "aws_instance" "nat" {
+  ami                         = "ami-00a9d4a05375b2763" # Amazon Linux 2 NAT AMI (make sure it's updated)
+  instance_type               = "t3.nano"
+  subnet_id                   = aws_subnet.public[0].id
+  source_dest_check           = false
+  vpc_security_group_ids      = [aws_security_group.nat_sg.id]
+  associate_public_ip_address = true
 
-resource "aws_eip_association" "nat_eip_assoc" {
-  instance_id   = aws_instance.nat_instance.id
-  allocation_id = aws_eip.nat.id
+  tags = {
+    Name = "nat-instance"
+  }
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.terraform-vpc.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = merge(var.tags,
-    {
-      Name = "public-rt"
-    }
-  )
+
+  tags = {
+    Name = "eks-public-rt"
+  }
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.terraform-vpc.id
-  tags = merge(var.tags,
-    {
-      Name = "private-rt"
-    }
-  )
-}
+  vpc_id = aws_vpc.main.id
 
-resource "aws_route" "private_nat_route" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  network_interface_id   = aws_instance.nat_instance.primary_network_interface_id
+  route {
+    cidr_block           = "0.0.0.0/0"
+    network_interface_id = aws_instance.nat.primary_network_interface_id
+  }
+
+  tags = {
+    Name = "eks-private-rt"
+  }
 }
 
 resource "aws_route_table_association" "public" {
@@ -138,4 +115,16 @@ resource "aws_route_table_association" "private" {
   count          = 2
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
 }
