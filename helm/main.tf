6dac001f7c8f3c950f957b0f1a3058509e0efcbc
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-06-04 01:29:36 -0400

    updated version

--- helm/main.tf
+++ helm/main.tf
@@ -255,7 +255,7 @@ resource "helm_release" "aws_load_balancer_controller" {
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
-  version    = "1.6.2"
+  version    = "1.13.2"
 
   set {
     name  = "clusterName"
