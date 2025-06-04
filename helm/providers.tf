a46afb18b56b0f641d371c9649c4f667cdef1028
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2025-06-04 01:02:04 -0400

    provider updated

--- helm/providers.tf
+++ helm/providers.tf
@@ -32,15 +32,22 @@ provider "aws" {
 # }
 
 # Configure Helm provider
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    # token                  = data.aws_eks_cluster_auth.cluster.token
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", "eks-demo-cluster-01"]
-    }
-  }
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#     # token                  = data.aws_eks_cluster_auth.cluster.token
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       command     = "aws"
+#       args        = ["eks", "get-token", "--cluster-name", "eks-demo-cluster-01"]
+#     }
+#   }
+# }
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
+
